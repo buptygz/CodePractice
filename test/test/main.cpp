322213d4ba1801ea73fc74d8b6ceedf1e9e56da5
於光中 <yuguangzhong@MacBook-Pro.local>
2017-09-06 15:34:07 +0800

    test

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 於光中 on 2017/9/6.
+//  Copyright © 2017年 於光中. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
